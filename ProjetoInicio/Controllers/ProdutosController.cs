175b00d056905f7fb00422bfc33d057f6e04ba16
Aluno.Etec <aula.etec.com>
2026-06-11 14:43:28 -0300

    Continuação do projeto - TA

--- ProjetoInicio/Controllers/ProdutosController.cs
+++ ProjetoInicio/Controllers/ProdutosController.cs
@@ -1,20 +1,37 @@
 using ProjetoInicio.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.Design;
 
 namespace ProjetoInicio.Controllers
 {
     public class ProdutosController : Controller
     {
-
         private List<Produto> produtos = new List<Produto>()
         {
             new Produto(1, "Notebook", 3000),
-            new Produto(2, "Mouse", 200)
+            new Produto(2, "Mouse", 200),
+            new Produto(3, "Teclado", 800),
+            new Produto(4, "Gabinete", 500),
+            new Produto(5, "Celular", 1000)
         };
         
         public IActionResult Index()
         {
-            return View();
+            return View(produtos);
+        }
+
+        public IActionResult Criar()
+        {
+            return View(); 
+        }
+
+        [HttpPost]
+        public IActionResult Criar(Produto produto)
+        {
+            produto.Id = produtos.Count + 1;
+            produtos.Add(produto);
+
+            return RedirectToAction("Index");
         }
     }
 }
